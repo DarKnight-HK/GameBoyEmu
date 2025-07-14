e48e4aacae0ce46f009387161deed550dcc8b00b
DarKnight-HK <ha986523@gmail.com>
2025-07-14 21:20:13 +0500

    Added fake LoadROM function

--- memory.cpp
+++ memory.cpp
@@ -1,6 +1,7 @@
 #include "memory.h"
+#include <algorithm>
 #include <cstdint>
-
+#include <vector>
 Memory::Memory() : m_memory(MEMORY_SIZE, 0) {}
 
 uint8_t Memory::readByte(uint16_t address) {
@@ -44,4 +45,27 @@ void Memory::writeWord(uint16_t address, uint16_t value) {
   // - low_byte contains the lower 8 bits (bits 0-7)
   writeByte(address, static_cast<uint8_t>(value & 0xFF));
   writeByte(address + 1, static_cast<uint8_t>((value >> 8) & 0xFF));
+}
+
+/**
+ * @brief Loads a ROM (Read-Only Memory) into the memory.
+ *
+ * For now, this will simply copy the ROM data into the beginning of
+ * our simulated memory. In a real emulator, this would involve
+ * handling cartridge headers and Memory Bank Controllers (MBCs).
+ *
+ * @param romData A vector of bytes representing the ROM content.
+ */
+
+void Memory::loadROM(const std::vector<uint8_t> &romData) {
+  if (romData.size() >= MEMORY_SIZE) {
+    std::cerr << "ROM Size is larger than memory size" << std::endl;
+  }
+  size_t bytesToCopy =
+      std::min(romData.size(), static_cast<size_t>(MEMORY_SIZE));
+  for (size_t i = 0; i < bytesToCopy; ++i) {
+    m_memory[i] = romData[i];
+  }
+  std::cout << "ROM loaded successfully. Size: " << bytesToCopy << " bytes."
+            << std::endl;
 }
