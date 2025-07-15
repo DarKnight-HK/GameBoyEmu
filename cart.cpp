bc4c493aa27b282bf4ec64de4ab00459ca7f846b
DarKnight-HK <ha986523@gmail.com>
2025-07-15 14:37:27 +0500

    Added Checksum Calculation

--- cart.cpp
+++ cart.cpp
@@ -1,5 +1,6 @@
 #include "cart.h"
 #include "constants.h"
+#include <cstdint>
 #include <cstring>
 #include <fstream>
 #include <ios>
@@ -173,4 +174,23 @@ bool Cart::load_cart(std::string cart) {
   // Convert the binary data to vector<uint8_t>
   file.read(reinterpret_cast<char *>(this->romData.data()), size);
   parseHeader();
+
+  std::cout << "Cartridge Loaded\n";
+  std::cout << "Title    : " << this->header->getTitle();
+  std::cout << "Type     : " << this->header->getCartTypeString();
+  std::cout << "ROM Size : " << this->header->getRomSize();
+  std::cout << "RAM Size : " << this->header->getRamSize();
+  std::cout << "LIC Code : " << this->header->getLicCodeString();
+  std::cout << "ROM Vers : " << this->header->getVersion();
+
+  uint16_t x = 0;
+  for (uint16_t i = 0x0134; i <= 0x014C; i++) {
+    x = x - this->romData[i] - 1;
+  }
+
+  std::cout << "Checksum : " << this->header->getChecksum() << "\n";
+  (x & 0xFF) ? std::cout << "PASSED" : std::cout << "FAILED";
+  std::cout << std::endl;
+
+  return true;
 }
